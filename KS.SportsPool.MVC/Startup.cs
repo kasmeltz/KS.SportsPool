7369d8b0f1e7a70ce24cb9e68fa26a4c8d82f7ad
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-03 18:25:30 -0500

    Initial check in

---
+++ KS.SportsPool.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KS.SportsPool.MVC.Startup))]
+namespace KS.SportsPool.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
