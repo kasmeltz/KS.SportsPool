52fe6df1241f83d76bcd8aee395bd1369dae8f25
kasmeltz <kasmeltz@lakeheadu.ca>
2016-04-04 11:27:09 -0500

    UI WIP

--- KS.SportsPool.MVC/Controllers/AdminController.cs
+++ KS.SportsPool.MVC/Controllers/AdminController.cs
@@ -84,6 +84,9 @@ namespace KS.SportsPool.MVC.Controllers
         public async Task<ActionResult> Athletes()
         {
             ViewBag.Title = UIUtilities.SiteTitle;
+            ViewBag.Success = TempData["Success"];
+            ViewBag.Error = TempData["Error"];
+            ViewBag.ScrollSection = TempData["ScrollSection"];
 
             IEnumerable<Team> teams = await Repository
                 .Teams()
@@ -96,33 +99,35 @@ namespace KS.SportsPool.MVC.Controllers
             return View(new AthleteListViewModel { Athletes = athletes, Teams = teams });
         }
 
+        public async Task<ActionResult> DeleteAthlete(int id)
+        {
+            try
+            {
+                await Repository.Athletes().Delete(id);
+                TempData["Success"] = "The athlete was deleted successfully!";
+                return RedirectToAction("Athletes");
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "There was an error deleting the athlete!";
+                return RedirectToAction("Athletes");
+            }
+        }
 
         [HttpPost]
         public async Task<ActionResult> AddAthlete(Athlete athleteToAdd)
         {
-            IEnumerable<Team> teams = await Repository
-                .Teams()
-                .List(DateTime.Now.Year);
-
-            IEnumerable<Athlete> athletes = null;
-
             try
             {
                 athleteToAdd.Year = DateTime.Now.Year;
                 await Repository.Athletes().Insert(athleteToAdd);
-                athletes = await Repository
-                    .Athletes()
-                    .List(DateTime.Now.Year);
-                @ViewBag.Success = "The athlete was added successfully!";
-                return PartialView("AthleteList", new AthleteListViewModel { Athletes = athletes, Teams = teams });
+                TempData["Success"] = "The athlete '" + athleteToAdd.FirstName + " "+ athleteToAdd.LastName + "' was added successfully!";
+                return RedirectToAction("Athletes");
             }
             catch (Exception ex)
             {
-                athletes = await Repository
-                    .Athletes()
-                    .List(DateTime.Now.Year);
-                @ViewBag.Error = "There was an error adding the athlete!";
-                return PartialView("AthleteList", new AthleteListViewModel { Athletes = athletes, Teams = teams });
+                TempData["Error"] = "There was an error adding the athlete '" + athleteToAdd.FirstName + " " + athleteToAdd.LastName + "'!";
+                return RedirectToAction("Athletes");
             }
         }
 
@@ -133,11 +138,15 @@ namespace KS.SportsPool.MVC.Controllers
             {
                 athleteToUpdate.Year = DateTime.Now.Year;
                 await Repository.Athletes().Update(athleteToUpdate);
-                return Content("The athlete was saved successfully");
+                TempData["Success"] = "The athlete '" + athleteToUpdate.FirstName + " " + athleteToUpdate.LastName + "' was updated successfully!";
+                TempData["ScrollSection"] = "AthleteDiv" + athleteToUpdate.Id;
+                return RedirectToAction("Athletes");
             }
             catch (Exception ex)
             {
-                return Content("There was an error updating the athlete!");
+                TempData["Error"] = "There was an error updating the athlete '" + athleteToUpdate.FirstName + " " + athleteToUpdate.LastName + "'!";
+                TempData["ScrollSection"] = "AthleteDiv" + athleteToUpdate.Id;
+                return RedirectToAction("Athletes");
             }
         }
 
---
+++ KS.SportsPool.MVC/Models/AthleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using KS.SportsPool.Data.POCO;
+using System.Collections.Generic;
+
+namespace KS.SportsPool.MVC.Models
+{
+    public class AthleteViewModel
+    {
+        public Athlete Athlete { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+    }
+}
